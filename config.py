7c13d288c502d53c3771145fca44a843534faf58
Andrew Heekin <andrewheekin@gmail.com>
2015-09-08 12:36:16 -0700

    limit broker pool

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class BaseConfig(object):
 	CELERY_TASK_SERIALIZER = 'json'
 	BROKER_URL = os.environ.get('RABBITMQ_BIGWIG_URL','amqp://localhost:5672')
+	BROKER_POOL_LIMIT = 1
 	MONGO_URI = os.environ.get('MONGOLAB_URI','mongodb://localhost:27017/')
 	CLIENT_KEY="iFxuEMWv8110vLIWWqA54w"
 	CLIENT_SECRET="rC0cr1w0heC6swvuDTDIiE98ipNvH6WiEI62YzeC0k"
