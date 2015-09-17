8cbf237b0a0db82b7b48ace7b45f28ee42cf021d
Andrew Heekin <andrewheekin@gmail.com>
2015-09-17 12:56:27 -0400

    added mongotweets

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn app:app
-worker: celery worker -A tasks.celery -l INFO
--- app.py
+++ app.py
@@ -1,14 +1,53 @@
 from flask import Flask, render_template, jsonify
-from tasks import hello
-
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+from pymongo import MongoClient
+import time
+import json
+import os
 
 app = Flask(__name__)
+
+MONGOLAB_URI = os.environ.get('MONGOLAB_URI')
+
+ckey="iFxuEMWv8110vLIWWqA54w"
+csecret="rC0cr1w0heC6swvuDTDIiE98ipNvH6WiEI62YzeC0k"
+atoken="991518511-9QlULoonEjUs5G3M7Mhc1m3iAQPISRqYtitXrmHh"
+asecret="72ggRW5sY7CCAHRaCjSBx6tGcm7fbPrZ9CB7EPc94"
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+class listener(StreamListener):
+
+ def on_data(self, data):
+
+     # client = MongoClient('localhost', 27017)
+   	 client = MongoClient(MONGOLAB_URI)
+   	 db = client.get_default_database()
+     # db = client['twitter_db']
+     collection = db['twitter_collection']
+     tweet = json.loads(data)
+
+     collection.insert(tweet)
+
+     return True
+
+ def on_error(self, status):
+     print status
+
 
 
 @app.route('/')
 def index():
-	hello.delay()
+	twitterStream = Stream(auth, listener())
+	twitterStream.filter(track=["#braves"])
 	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
+
+
