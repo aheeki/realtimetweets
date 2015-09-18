02cdf5772f20f41884b9189894d348ebe035592a
Andrew Heekin <andrewheekin@gmail.com>
2015-09-18 15:32:15 -0400

    testing

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def track(hashtag):
 	if (hashtag[:1] != '#'):
 		hashtag = '#' + hashtag
 	# hello.revoke()
-	result = hello.delay(hashtag)
+	hello.delay(hashtag)
 	return render_template('index.html', hashtag=hashtag)
 
 if __name__ == '__main__':
