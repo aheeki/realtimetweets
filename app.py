d1a666c9bc244fbb896be763e59a8f5fa9d9d684
Andrew Heekin <andrewheekin@gmail.com>
2015-09-21 16:16:55 -0400

    task status

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, jsonify, request
-from celery.task.control import revoke
 import os
 
 app = Flask(__name__)
@@ -26,20 +25,54 @@ def track():
 		print('result didnt work')
 
 	try:
-		print('asyncresult',AsyncResult(result))
+		AsyncResult(result)
+		print('asyncresult')
 	except:
 		print('asyncresult didnt work')
+
 	try:
-		print('asyncresultstate',AsyncResult(result).state)
+		AsyncResult(result).state
+		print('asyncresultstate')
 	except:
 		print('asyncresultstate didnt work')
+
 	try:
-		print('revoking',revoke(result,terminate=True))
+		revoke(result,terminate=True)
+		print('revoking')
 	except:
 		print('revoking didnt work')
-	revoke(result,terminate=True)
-
 	return render_template('track.html', hashtag=hashtag)
+
+
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = long_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        # job did not start yet
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
 
 
 if __name__ == '__main__':
